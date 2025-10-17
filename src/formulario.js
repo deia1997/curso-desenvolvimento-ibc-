1bd87fae78e4a6eaa7389619e1df0ddecde7ae19
Deia <ddeiadrew@gmail.com>
2025-10-17 00:39:49 -0300

    start

---
+++ src/formulario.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const form_js_1 = require("./classes/form.js");
+const formContato = document.getElementById("formContato");
+const txtNome = document.getElementById("txtNome");
+const txtEmail = document.getElementById("txtEmail");
+const txtMensagem = document.getElementById("txtMensagem");
+const btnEnviar = document.getElementById("btnEnviar");
+const btnLimpar = document.getElementById("btnLimpar");
+const divMensagem = document.getElementById("divMensagem");
+function exibirMensagem(color, msg) {
+    divMensagem.style.color = color;
+    divMensagem.textContent = msg;
+}
+formContato.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const nome = txtNome.value.trim();
+    const email = txtEmail.value.trim();
+    const mensagem = txtMensagem.value.trim();
+    if (!nome || !email || !mensagem) {
+        exibirMensagem("red", "Todos os campos são obrigatórios!");
+        return;
+    }
+    const novoForm = new form_js_1.Form(nome, email, mensagem);
+    novoForm.cadastrar();
+    exibirMensagem("green", "Mensagem enviada com sucesso!");
+    formContato.reset();
+});
+btnLimpar.addEventListener("click", (event) => {
+    event.preventDefault();
+    formContato.reset();
+    divMensagem.textContent = "";
+});
