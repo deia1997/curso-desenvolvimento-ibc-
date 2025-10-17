f7c32cdd8bfb1d13583da92dc9e3d1ff38d702c8
deia1997 <ddeiadrew@gmail.com>
2025-10-17 10:12:10 -0300

    Update form.ts

--- src/classes/form.ts
+++ src/classes/form.ts
@@ -22,22 +22,6 @@ export class Form {
     return JSON.parse(localStorage.getItem("listaForm") || "[]");
   }
 
-  static buscar(id: string): Form | undefined {
-    const lista = Form.listar();
-    return lista.find(form => form.id === id);
-  }
-
-  static alterar(formAlterado: Form): void {
-    let listaForm: Form[] = JSON.parse(localStorage.getItem("listaForm") || "[]");
-    listaForm = listaForm.map(form => {
-      if (form.id === formAlterado.id) {
-        return formAlterado;
-      }
-      return form;
-    });
-    localStorage.setItem("listaForm", JSON.stringify(listaForm));
-  }
-
   static excluir(id: string): void {
     let listaForm: Form[] = JSON.parse(localStorage.getItem("listaForm") || "[]");
     listaForm = listaForm.filter(form => form.id !== id);
