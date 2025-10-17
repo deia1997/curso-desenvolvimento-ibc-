ee8945e835d71a602e9d0215406b8dc3fa1b15d2
deia1997 <ddeiadrew@gmail.com>
2025-10-17 01:12:31 -0300

    Update form.js

--- src/classes/form.js
+++ src/classes/form.js
@@ -1,44 +1,24 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Form = void 0;
-class Form {
-    constructor(nome, email, mensagem) {
-        this.nome = nome;
-        this.email = email;
-        this.mensagem = mensagem;
-        this.id = crypto.randomUUID();
-        this.nome = nome;
-        this.email = email;
-        this.mensagem = mensagem;
-    }
-    cadastrar() {
-        const listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
-        listaForm.push(this);
-        localStorage.setItem("listaForm", JSON.stringify(listaForm));
-    }
-    static listar() {
-        const listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
-        return listaForm;
-    }
-    static excluir(id) {
-        let listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
-        listaForm = listaForm.filter((form) => form.id !== id);
-        localStorage.setItem("listaForm", JSON.stringify(listaForm));
-    }
-    static alterar(formAlterado) {
-        let listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
-        listaForm = listaForm.map((form) => {
-            if (form.id === formAlterado.id) {
-                return formAlterado;
-            }
-            return form;
-        });
-        localStorage.setItem("listaForm", JSON.stringify(listaForm));
-    }
-    static buscar(id) {
-        let listaForm = this.listar();
-        let form = listaForm.find(form => form.id === id);
-        return form;
-    }
+export class Form {
+  constructor(nome, email, mensagem) {
+    this.id = crypto.randomUUID();
+    this.nome = nome;
+    this.email = email;
+    this.mensagem = mensagem;
+  }
+
+  cadastrar() {
+    const listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
+    listaForm.push(this);
+    localStorage.setItem("listaForm", JSON.stringify(listaForm));
+  }
+
+  static listar() {
+    return JSON.parse(localStorage.getItem("listaForm") || "[]");
+  }
+
+  static excluir(id) {
+    let listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
+    listaForm = listaForm.filter(f => f.id !== id);
+    localStorage.setItem("listaForm", JSON.stringify(listaForm));
+  }
 }
-exports.Form = Form;
