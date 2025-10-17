ad188c517baa2616e58801e8a5c6d05f7220c036
deia1997 <ddeiadrew@gmail.com>
2025-10-17 02:45:47 -0300

    Update form.js

--- src/classes/form.js
+++ src/classes/form.js
@@ -1,24 +1,35 @@
 export class Form {
-  constructor(nome, email, mensagem) {
-    this.id = crypto.randomUUID();
-    this.nome = nome;
-    this.email = email;
-    this.mensagem = mensagem;
-  }
-
-  cadastrar() {
-    const listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
-    listaForm.push(this);
-    localStorage.setItem("listaForm", JSON.stringify(listaForm));
-  }
-
-  static listar() {
-    return JSON.parse(localStorage.getItem("listaForm") || "[]");
-  }
-
-  static excluir(id) {
-    let listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
-    listaForm = listaForm.filter(f => f.id !== id);
-    localStorage.setItem("listaForm", JSON.stringify(listaForm));
-  }
+    constructor(nome, email, mensagem) {
+        this.nome = nome;
+        this.email = email;
+        this.mensagem = mensagem;
+        this.id = crypto.randomUUID();
+    }
+    cadastrar() {
+        const listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
+        listaForm.push(this);
+        localStorage.setItem("listaForm", JSON.stringify(listaForm));
+    }
+    static listar() {
+        return JSON.parse(localStorage.getItem("listaForm") || "[]");
+    }
+    static buscar(id) {
+        const lista = Form.listar();
+        return lista.find(form => form.id === id);
+    }
+    static alterar(formAlterado) {
+        let listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
+        listaForm = listaForm.map(form => {
+            if (form.id === formAlterado.id) {
+                return formAlterado;
+            }
+            return form;
+        });
+        localStorage.setItem("listaForm", JSON.stringify(listaForm));
+    }
+    static excluir(id) {
+        let listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
+        listaForm = listaForm.filter(form => form.id !== id);
+        localStorage.setItem("listaForm", JSON.stringify(listaForm));
+    }
 }
