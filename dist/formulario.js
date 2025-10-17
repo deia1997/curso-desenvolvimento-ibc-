bde029a35d9d7b952a94541d709c73ea72318518
deia1997 <ddeiadrew@gmail.com>
2025-10-17 14:09:23 -0300

    Update formulario.js

--- dist/formulario.js
+++ dist/formulario.js
@@ -1,32 +1,60 @@
 import { Form } from "./classes/form.js";
-const formContato = document.getElementById("formContato");
+
+const formContato = document.getElementById("formulario");
 const txtNome = document.getElementById("txtNome");
 const txtEmail = document.getElementById("txtEmail");
 const txtMensagem = document.getElementById("txtMensagem");
-const btnEnviar = document.getElementById("btnEnviar");
-const btnLimpar = document.getElementById("btnLimpar");
 const divMensagem = document.getElementById("divMensagem");
+
+let params = new URLSearchParams(window.location.search);
+let id = params.get("id");
+
+window.onload = () => {
+    if (id) {
+        const btns = formContato.querySelectorAll("#btnSubmit");
+        btns[0].textContent = "Alterar";
+        carregarDadosForm(id);
+    }
+};
+
 function exibirMensagem(color, msg) {
     divMensagem.style.color = color;
     divMensagem.textContent = msg;
 }
+
 formContato.addEventListener("submit", (event) => {
     event.preventDefault();
-    const nome = txtNome.value.trim();
-    const email = txtEmail.value.trim();
-    const mensagem = txtMensagem.value.trim();
-    if (!nome || !email || !mensagem) {
-        exibirMensagem("red", "Todos os campos são obrigatórios!");
+    const target = event.submitter;
+
+    if (target.textContent === "Limpar") {
+        txtNome.value = "";
+        txtEmail.value = "";
+        txtMensagem.value = "";
+        divMensagem.textContent = "";
         return;
     }
-    const novoForm = new Form(nome, email, mensagem);
-    novoForm.cadastrar();
-    exibirMensagem("green", "Mensagem enviada com sucesso!");
-    formContato.reset();
+
+    const nome = txtNome.value;
+    const email = txtEmail.value;
+    const mensagem = txtMensagem.value;
+
+    if (!id) {
+        const form = new Form(nome, email, mensagem);
+        form.cadastrar();
+        exibirMensagem("green", "Formulário enviado com sucesso");
+    } else {
+        const formAlterado = new Form(nome, email, mensagem);
+        formAlterado.id = id;
+        Form.alterar(formAlterado);
+        exibirMensagem("green", "Alteração realizada com sucesso");
+    }
 });
-btnLimpar.addEventListener("click", (event) => {
-    event.preventDefault();
-    formContato.reset();
-    divMensagem.textContent = "";
-});
-//# sourceMappingURL=formulario.js.map
+
+function carregarDadosForm(id) {
+    const form = Form.buscarForm(id);
+    if (form) {
+        txtNome.value = form.nome;
+        txtEmail.value = form.email;
+        txtMensagem.value = form.mensagem;
+    }
+}
