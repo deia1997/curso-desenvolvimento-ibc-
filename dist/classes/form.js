0f61ecb2ff71ea284480a58c7e6a9dd1b4ace5fa
deia1997 <ddeiadrew@gmail.com>
2025-10-17 16:51:28 -0300

    Update form.js

--- dist/classes/form.js
+++ dist/classes/form.js
@@ -5,24 +5,29 @@ export class Form {
         this.mensagem = mensagem;
         this.id = crypto.randomUUID();
     }
+
     cadastrar() {
         const listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
         listaForm.push(this);
         localStorage.setItem("listaForm", JSON.stringify(listaForm));
     }
+
     static listar() {
         return JSON.parse(localStorage.getItem("listaForm") || "[]");
     }
+
     static excluir(id) {
         let listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
         listaForm = listaForm.filter((form) => form.id !== id);
         localStorage.setItem("listaForm", JSON.stringify(listaForm));
     }
+
     static alterar(formAlterado) {
         let listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
         listaForm = listaForm.map((form) => form.id === formAlterado.id ? formAlterado : form);
         localStorage.setItem("listaForm", JSON.stringify(listaForm));
     }
+
     static buscarForm(id) {
         const listaForm = this.listar();
         return listaForm.find((form) => form.id === id);
