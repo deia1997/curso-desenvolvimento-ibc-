cb567c92711aa852c0f32a4450a687269f488879
deia1997 <ddeiadrew@gmail.com>
2025-10-23 14:13:48 -0300

    Update AreaRestrita.js

--- src/classes/AreaRestrita.js
+++ src/classes/AreaRestrita.js
@@ -1,27 +1,24 @@
 export class AreaRestrita {
-  public id: string;
-
-  constructor(
-    public nome: string,
-    public email: string,
-    public mensagem: string
-  ) {
+  constructor(nome, email, mensagem) {
+    this.nome = nome;
+    this.email = email;
+    this.mensagem = mensagem;
     this.id = crypto.randomUUID();
   }
 
-  cadastrar(): void {
-    const lista: AreaRestrita[] = JSON.parse(localStorage.getItem("listaAreaRestrita") || "[]");
+  cadastrar() {
+    const lista = JSON.parse(localStorage.getItem("listaAreaRestrita") || "[]");
     lista.push(this);
     localStorage.setItem("listaAreaRestrita", JSON.stringify(lista));
   }
 
-  static listar(): AreaRestrita[] {
-    const lista: AreaRestrita[] = JSON.parse(localStorage.getItem("listaAreaRestrita") || "[]");
+  static listar() {
+    const lista = JSON.parse(localStorage.getItem("listaAreaRestrita") || "[]");
     return lista;
   }
 
-  static excluir(id: string): void {
-    let lista: AreaRestrita[] = JSON.parse(localStorage.getItem("listaAreaRestrita") || "[]");
+  static excluir(id) {
+    let lista = JSON.parse(localStorage.getItem("listaAreaRestrita") || "[]");
     lista = lista.filter(area => area.id !== id);
     localStorage.setItem("listaAreaRestrita", JSON.stringify(lista));
   }
